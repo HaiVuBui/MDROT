69eb4b6dfc3c99db28ff1530a10f186a3a2ac0f3
Gandalf <gandalf@MinasTirith>
2024-10-31 12:37:18 +0700

    latest

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python Experiment_mnist.py --max_iter 20000 &
+python Experiment_mnist.py --max_iter 50000 &
 
-#python Experiment.py --max_iter 20000 --size 20 &
+python Experiment.py --max_iter 50000 --size 60 &
 
 wait
