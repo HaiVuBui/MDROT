f03d43099b2e4d2f17a31d98d2721058e82a261f
haivubui <buivuhai1105@gmail.com>
2025-05-23 10:36:54 +0000

    Try jax for sinkhorn
    Rewrite multisinkhorn with jax (and torch)

--- test.py
+++ test.py
@@ -2,21 +2,43 @@ import numpy as np
 from src.MDrot import Mdrot_gpu
 from src.prepare_data import prepare_input
 
+#size = 60
+#exp_idx = 0
+#max_iter = 1000
+#ep = 1e-5
+#
+#data_folder = f'data/size{size}/seed20/'
+#X = np.fromfile(data_folder + f'exp_number{exp_idx}.npy', dtype=np.float64)
+#
+#Cost, p, q, s = prepare_input(k=exp_idx,N=10,size=size)
+#x0 = np.tensordot(p, np.tensordot(q, s, 0), 0)
+#opt = (X * Cost.reshape(-1)).sum().item()
+#
+#
+#
+#out = Mdrot_gpu(x0, Cost, p, q, s,opt=X ,max_iters=max_iter, step=ep, compute_r_primal=True, eps_abs=1e-15, verbose=False, print_every=100)
+#
+#print(out['sol'].shape)
+#print("done")
+
+
+from src.algos import solve_multi_sinkhorn, solve_rrsinkhorn, solve_multi_greenkhorn, solve_pd_aam
+
 size = 60
 exp_idx = 0
-max_iter = 1000
-ep = 1e-5
+max_iter = 10000
+ep = 1e-2
 
 data_folder = f'data/size{size}/seed20/'
 X = np.fromfile(data_folder + f'exp_number{exp_idx}.npy', dtype=np.float64)
 
+# Prepare the input data
 Cost, p, q, s = prepare_input(k=exp_idx,N=10,size=size)
 x0 = np.tensordot(p, np.tensordot(q, s, 0), 0)
 opt = (X * Cost.reshape(-1)).sum().item()
 
+# Combine p, q, and s into target_mu
+target_mu = np.concatenate([p, q, s], axis=0)
 
 
-out = Mdrot_gpu(x0, Cost, p, q, s,opt=X ,max_iters=max_iter, step=ep, compute_r_primal=True, eps_abs=1e-15, verbose=False, print_every=100)
-
-print(out['sol'].shape)
-print("done")
+temp = solve_multi_sinkhorn(Cost, target_mu, epsilon=ep, max_iter=max_iter)
